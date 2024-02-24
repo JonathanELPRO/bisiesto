b2618ea36b4fb46d9c747be7ec5f6d9d928b4faa
Jonathan <jonathan.moruno@ucb.edu.bo>
2024-02-24 17:20:44 -0400

    Caso de prueba para generar el texto 'Es bisiesto' solo sabiendo si el numero es divisible por 4 pero no por 100, probando el caso numero 2016

--- src/bisiestoYorN.js
+++ src/bisiestoYorN.js
@@ -7,6 +7,9 @@ function determinarBisiesto(anio) {
   {
     return "No es bisiesto"
   }
+  if (anio%4===0 && anio%100!==0){
+    return "Es bisiesto"
+  }
 
 
 }
--- src/bisiestoYorN.spec.js
+++ src/bisiestoYorN.spec.js
@@ -7,4 +7,8 @@ describe("BisiestoYorN", () => {
   it("Deberia retornar que el año ingresado no es bisiesto solamente sabiendo que es divisible por 100 pero no por 400", () => {
     expect(determinarBisiesto(2100)).toEqual("No es bisiesto");
   });
+  it("Deberia retornar que el año ingresado es bisiesto solamente sabiendo que es divisible por 4 pero no por 100", () => {
+    expect(determinarBisiesto(2016)).toEqual("Es bisiesto");
+    
+  });
 });
