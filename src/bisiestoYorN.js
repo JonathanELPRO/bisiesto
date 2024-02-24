f58a3325f0ac841bd90377e8dbcda88de47be577
Jonathan <jonathan.moruno@ucb.edu.bo>
2024-02-24 17:08:55 -0400

    Caso de prueba para generar el texto 'No es bisiesto' solo sabiendo que el numero es divisble por 100 pero no por 400, probando el caso numero 2100

--- src/bisiestoYorN.js
+++ src/bisiestoYorN.js
@@ -1,8 +1,13 @@
 function determinarBisiesto(anio) {
-  if (anio%400===0){
+  if (anio%400===0)
+  {
     return "Es bisiesto"
   }
-  
+  if (anio%100===0 && anio%400!==0){
+    return "No es bisiesto"
+  }
+
+
 }
 
 export default determinarBisiesto;
--- src/bisiestoYorN.spec.js
+++ src/bisiestoYorN.spec.js
@@ -4,4 +4,7 @@ describe("BisiestoYorN", () => {
   it("Deberia retornar que el año ingresado es bisiesto solamente sabiendo si es divisible por 400", () => {
     expect(determinarBisiesto(800)).toEqual("Es bisiesto");
   });
+  it("Deberia retornar que el año ingresado no es bisiesto solamente sabiendo que es divisible por 100 pero no por 400", () => {
+    expect(determinarBisiesto(2100)).toEqual("No es bisiesto");
+  });
 });
